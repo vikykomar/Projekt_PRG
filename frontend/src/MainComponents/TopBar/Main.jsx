3937943dee4e7150c29688a24f1e4949a5aef4f0
Viktor Komárek <vikykomar@seznam.cz>
2026-01-14 14:16:54 +0100

    Jednoduchý návrh TopBaru

--- frontend/src/MainComponents/TopBar/Main.jsx
+++ frontend/src/MainComponents/TopBar/Main.jsx
@@ -1,11 +1,21 @@
-import { Outlet } from "react-router";
+import { NavLink, Outlet } from "react-router";
 
 import Style from "./Main.module.css"
+
+import ShoppingCartIcon from "../../Icons/ShoppingCart.svg"
 
 export default function TopBar(){
 
     return (
         <>
+            <div className={Style.Main_Bar}>
+                <div className={Style.Logo_Container}>
+                    <h1>Nejlepší Eshop</h1>
+                </div>
+                <div className={Style.Kosik_Container}>
+                    <NavLink to={"/Kosik"}>Košík</NavLink>
+                </div>
+            </div>
             <Outlet/>
         </>
     )
