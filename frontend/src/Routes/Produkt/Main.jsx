b70862e2d6fd1c7a65c974a919c68636a2d528f2
Viktor Komárek <vikykomar@seznam.cz>
2026-02-08 20:42:19 +0100

    Přidáno přepínání obrázků

--- frontend/src/Routes/Produkt/Main.jsx
+++ frontend/src/Routes/Produkt/Main.jsx
@@ -15,6 +15,8 @@ export default function Produkt(){
     const [Produkt, set_Produkt] = useState([null, {Nazev: null, Sklad: null, Cena: null, Description: null, Short_Description: null, IMG_Preview: null, IMG: []}])
     const Pocet_V_Kosiku = useMemo(() => {if(params.UUID === undefined) return 0; const Pocet = use_Kosik.Kosik.get(params.UUID); return Pocet === undefined ? 0 : Pocet}, [use_Kosik.Kosik, params.UUID])
 
+    const [Current_IMG, set_Current_IMG] = useState(0)
+
     useEffect(() => {
         if(params.UUID === undefined){
             return
@@ -38,10 +40,36 @@ export default function Produkt(){
         use_Kosik.Odebrat(Produkt[0], 1)    
     }
 
+    const IMG_Predchozi = () => {
+        set_Current_IMG((Previous_Current_IMG) => {
+            if(Previous_Current_IMG <= 0){
+                return Produkt[1].IMG.length - 1
+            }
+
+            return Previous_Current_IMG - 1
+        })
+    }
+
+    const IMG_Dalsi = () => {
+        set_Current_IMG((Previous_Current_IMG) => {
+            if(Previous_Current_IMG + 1 >= Produkt[1].IMG.length){
+                return 0
+            }
+
+            return Previous_Current_IMG + 1
+        })
+    }
+
     return(
         <div className="Content">
             <div className={Style.Produkt}>
-                <img src={`/Produkty_Img/${Produkt[1].IMG[0]}`}/>
+                <div className={Style.IMG_Container}>
+                    <img src={`/Produkty_Img/${Produkt[1].IMG[Current_IMG]}`}/>
+
+
+                    <input className={`${Style.Navigace} ${Style.Predchozi}`} type="button" value={"‹"} onClick={IMG_Predchozi}/>
+                    <input className={`${Style.Navigace} ${Style.Dalsi}`} type="button" value={"›"} onClick={IMG_Dalsi}/>
+                </div>
                 <div className={Style.Info}>
                     <h1>{Produkt[1].Nazev}</h1>
                     <h3 className={Produkt[1].Sklad <= 0 ? Style.Vyprodano : Style.Skladem}>{Produkt[1].Sklad <= 0 ? "Vyprodáno" : `Skladem: ${Produkt[1].Sklad} Ks`}</h3>
